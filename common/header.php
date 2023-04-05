4d6f0cbb578c9a08b964bc68ecff686d7848cdd8
iemvish <iemvishal.07@gmail.com>
2023-04-04 21:47:01 -0400

    change

---
+++ common/header.php
@@ -0,0 +1,72 @@
+<div class="header-top-bar py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-lg-8 col-md-8">
+        <ul class="contact-list m-0 p-0 me-auto">
+        <li><a href="mailto: info@edifyingvoyages.com"><span><i class="fa-solid fa-envelope"></i>    </span>info@edifyingvoyages.com </a></li>
+        <li><a href=""><span><i class="fa-solid fa-phone"></i>  </span>+91-708-708-7356 </a></li>
+        </ul>
+      </div>
+      <div class="col-sm-12 col-lg-4 col-md-4">
+      <ul class="social-media-list m-0 p-0">
+              <li><a href="https://www.facebook.com/Ditstek"><i class="fa-brands fa-facebook-f"></i></a></li>
+              <li><a href="https://twitter.com/DitsTek"><i class="fa-brands fa-twitter"></i></a></li>
+              <li><a href="https://www.linkedin.com/company/ditstek-innovations"><i class="fa-brands fa-linkedin-in"></i></a></li>
+              <li><a href="https://www.youtube.com/c/ditstek"><i class="fa-brands fa-youtube"></i></a></li>
+              <li><a href="https://www.instagram.com/ditstek_innovations/"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
+<header class=" sticky-top bg-white">
+    <div class="container-xl">
+      <nav class="navbar navbar-expand-lg ">
+        <a class="navbar-brand" href="index.php"><img class="img-fluid" src="assets/images/logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active-blue" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./about-us.php">About us</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Services
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">SEO</a></li>
+                <li><a class="dropdown-item" href="#">PPC</a></li>
+                <li><a class="dropdown-item" href="#">GRAPHIC DESIGNER</a></li>
+
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="portfolio.php">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blogs.php">Blogs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+            <li>
+              <a href="">
+                <button type="button" class="btn ms-3 secondary-solid-btn">get free proposal</button>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+
+      </nav>
+    </div>
+  </header>
