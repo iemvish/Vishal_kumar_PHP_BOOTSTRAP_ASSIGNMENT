4da437a880cfbb46df3792aa90d552a3ec347cb4
iemvishal.07 <iemvishal.07@gmail.com>
2023-05-01 18:56:10 +0530

    create header and carousel

--- common/header.php
+++ common/header.php
@@ -1,73 +1,75 @@
-<div class="header-top-bar py-3">
-  <div class="container">
+<div class="top-header blue-light-color">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-12 d-sm-none d-md-block d-lg-block col-lg-8 col-md-8">
-        <ul class="contact-list m-0 p-0 me-auto">
-        <li><a href="mailto: info@edifyingvoyages.com"><span><i class="fa-solid fa-envelope"></i>    </span>info@edifyingvoyages.com </a></li>
-        <li><a href=""><span><i class="fa-solid fa-phone"></i>  </span>+91-708-708-7356 </a></li>
-        </ul>
+      <div class="col-md-6">
+        <p class="m-0 py-2 ps-5">
+        <i class="fa-regular fa-clock blue-color"></i>
+        <small> Opening Hours: Mon - Tues : 6.00 am - 10.00 pm, Sunday Closed </small>
+        </p>
       </div>
-      <div class="col-sm-12 col-lg-4 col-md-4">
-      <ul class="social-media-list m-0 p-0">
-              <li><a href="https://www.facebook.com/Ditstek"><i class="fa-brands fa-facebook-f"></i></a></li>
-              <li><a href="https://twitter.com/DitsTek"><i class="fa-brands fa-twitter"></i></a></li>
-              <li><a href="https://www.linkedin.com/company/ditstek-innovations"><i class="fa-brands fa-linkedin-in"></i></a></li>
-              <li><a href="https://www.youtube.com/c/ditstek"><i class="fa-brands fa-youtube"></i></a></li>
-              <li><a href="https://www.instagram.com/ditstek_innovations/"><i class="fa-brands fa-instagram"></i></a></li>
-            </ul>
+      <div class="col-md-6 text-end pe-0">
+        <div>
+          <div class="position-relative d-inline-flex align-item-center text-white top-shape bg-blue-color px-5 ">
+            <div class="border-end py-2">
+              <p class="m-0 me-3">
+              <i class="fa fa-envelope-open"></i> info@example.com
+              </p>
+            </div>  
+            <div class="py-2">
+              <p class="m-0 ms-3">
+              <i class="fa-solid fa-phone"></i> +012 345 6789
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-
 </div>
-<header class=" sticky-top bg-white">
-    <div class="container-xl">
-      <nav class="navbar navbar-expand-lg ">
-        <a class="navbar-brand" href="index.php"><img class="img-fluid" src="assets/images/logo.png" alt=""></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active-blue" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./about-us.php">About us</a>
-            </li>
 
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Services
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">SEO</a></li>
-                <li><a class="dropdown-item" href="#">PPC</a></li>
-                <li><a class="dropdown-item" href="#">social media</a></li>
-                <li><a class="dropdown-item" href="#">GRAPHIC DESIGNER</a></li>
+<header class="bg-light">
+<nav class="navbar navbar-expand-lg bg-light px-5 p-0 sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand p-0 " href="#">
+    <h1 class="blue-color m-0"> <i class="fa fa-tooth"></i> DentCare
+    </h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
 
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="portfolio.php">Portfolio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="blogs.php">Blogs</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact-us.php">Contact us</a>
-            </li>
-            <li>
-              <a href="">
-                <button type="button" class="btn ms-3 secondary-solid-btn">get free proposal</button>
-              </a>
-            </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pages
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Pricing Plan</a></li>
+            <li><a class="dropdown-item" href="#">Our Dentinst</a></li>
+            <li><a class="dropdown-item" href="#">Testonomial</a></li>
           </ul>
-
-        </div>
-
-      </nav>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Contact</a>
+        </li>
+        <li class="align-self-center px-4">
+          <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+        </li>
+      </ul>
+      <div>
+      <button type="button" class="btn header-btn">Appointment</button>
+      </div>
     </div>
-  </header>
+  </div>
+</nav>
+</header>
--- index.php
+++ index.php
@@ -6,142 +6,28 @@
   <title>bootstrap 5</title>
 </head>
 <body>
-  <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa-solid fa-angle-up"></i></button>
-  <!-- include header file-->
+  <!-- include header file -->
   <?php include "common/header.php" ?>
+ <!-- owl corosual -->
+ <div class="carousel">
+  <div class="owl-carousel owl-theme">
+    
+    <div class="item"><img src="assets/images/carousel-1.jpg" alt="">
+    <div class="carousel-caption">
+    <div class="p-3"></div>
+</div></div>
+<div class="item"><img src="assets/images/carousel.jpg" alt="">
+    <div class="carousel-caption">
 
-  <!-- owl carousel -->
-  <div class="carousel">
-    <div class="container">
-            <div class="owl-carousel owl-theme">
-              <div class="item">
-                  <div class="row">
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-content">
-                        <div class="banner-text">
-                          <strong class="text-white">Digital Marketing</strong>
-                          <h1 class="text-white">Build Your Brand with Leading Digital Marketing Agency</h1>
-                          <p class="text-white">Edifying Voyages is a creative digital marketing company that supports brands'
-                            glitter while transforming the businesses digitally for new growth opportunities.</p>
-                          <button type="button" class="btn btn-outline-light text-capitalize px-4">start now</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-image">
-                        <img src="assets/images/banner.png" class="img-fluid">
-                      </div>
-                    </div>
-                  </div>
-              </div> 
-              <div class="item">
-                  <div class="row">
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-content">
-                        <div class="banner-text">
-                          <strong class="text-white">Digital Marketing</strong>
-                          <h1 class="text-white">Build Your Brand with Leading Digital Marketing Agency</h1>
-                          <p class="text-white">Edifying Voyages is a creative digital marketing company that supports brands'
-                            glitter while transforming the businesses digitally for new growth opportunities.</p>
-                          <button type="button" class="btn btn-outline-light text-capitalize px-4">start now</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-image">
-                        <img src="assets/images/banner.png" class="img-fluid">
-                      </div>
-                    </div>
-                  </div>
-              </div> 
-              <div class="item">
-                  <div class="row">
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-content">
-                        <div class="banner-text">
-                          <strong class="text-white">Digital Marketing</strong>
-                          <h1 class="text-white">Build Your Brand with Leading Digital Marketing Agency</h1>
-                          <p class="text-white">Edifying Voyages is a creative digital marketing company that supports brands'
-                            glitter while transforming the businesses digitally for new growth opportunities.</p>
-                          <button type="button" class="btn btn-outline-light text-capitalize px-4">start now</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6 align-self-center">
-                      <div class="banner-image">
-                        <img src="assets/images/banner.png" class="img-fluid">
-                      </div>
-                    </div>
-                  </div>
-              </div> 
-            </div>
-        </div>
-      </div>
-  <!-- cards -->
-
-  <section class="card-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6 mt-sm-3 col-md-4">
-          <div class="card shadow-lg h-100">
-            <div class="card-image">
-              <img src="assets/images/growth.png" class="card-img-top " alt="...">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">guaranted growth</h5>
-              <p class="card-text">Helping buisneses to grow with our years of experiences that convert them more.</p>
-
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 mt-sm-3 col-md-4">
-          <div class="card shadow-lg h-100">
-            <div class="card-image">
-              <img src="assets/images/dedicated-team.png" class="card-img-top" alt="...">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">dedicated team</h5>
-              <p class="card-text">dedicated team to plan and develop the roadmap to delivery best outcomes</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 mt-sm-3 col-md-4 h-100">
-
-          <div class="card shadow-lg">
-            <div class="card-image">
-              <img src="assets/images/brand-building.png" class="card-img-top " alt="...">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">brand building</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's
-                content.</p>
-
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </section>
+  </div>
+  </div>
+ 
+ </div>
+ 
 
-  <!-- second banner -->
-  <section class="second-banner">
-    <img src="assets/images/sales-banner.webp" alt="">
-    <div class="centered ">
-      <h2>want more sales? </h2>
-      <p> <i>get your <u class="text-capitalize">free proposal</u> on how we can help you with that!</i>
-      </p>
-      <div class="second-banner-button">
-        <button type="button" class="btn outline-white-btn text-capitalize">get started now <span><i class="fa-solid fa-angles-right"></i></span></button>
-      </div>
-    </div>
-  </section>
 
-  <!-- Contact section -->
-  <?php include "common/contact-form.php" ?>
 
-  <!-- include footer file -->
-  <?php include "common/footer.php" ?>
   <?php include "common/common-js.php" ?>
 
 </body>
